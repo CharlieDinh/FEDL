1cbb72a17bd4523d851c5526049cc10daccfedfd
CharlieDinh <CharlieSex@156>
2020-06-26 11:27:02 +1000

    Rename file gennerate data

--- data/mnist/generate_niid_100users_bak.py
+++ data/mnist/generate_niid_100users_bak.py
@@ -7,8 +7,8 @@ import os
 
 random.seed(1)
 np.random.seed(1)
-NUM_USERS = 100  
-NUM_LABELS = 3
+NUM_USERS = 10
+NUM_LABELS = 2
 # Setup directory for train/test data
 train_path = './data/train/mnist_train.json'
 test_path = './data/test/mnist_test.json'
@@ -38,42 +38,35 @@ y = [[] for _ in range(NUM_USERS)]
 idx = np.zeros(10, dtype=np.int64)
 for user in range(NUM_USERS):
     for j in range(NUM_LABELS):  # 3 labels for each users
-        #l = (2*user+j)%10
-        l = (user + j) % 10
-        print("L:", l)
+        # l = (2*user+j)%10
+        l = (user + j) % 10 
+        print("L:",l)
         X[user] += mnist_data[l][idx[l]:idx[l]+10].tolist()
         y[user] += (l*np.ones(10)).tolist()
         idx[l] += 10
 
-print("IDX1:", idx)  # counting samples for each labels
+print("IDX1:", idx) # counting samples for each labels
 
 # Assign remaining sample by power law
 user = 0
 props = np.random.lognormal(
     0, 2., (10, NUM_USERS, NUM_LABELS))  # last 5 is 5 labels
-props = np.array([[[len(v)-1000]] for v in mnist_data]) * \
-    props/np.sum(props, (1, 2), keepdims=True)
 # print("here:",props/np.sum(props,(1,2), keepdims=True))
-#props = np.array([[[len(v)-100]] for v in mnist_data]) * \
-#    props/np.sum(props, (1, 2), keepdims=True)
+props = np.array([[[len(v)-100]] for v in mnist_data])*props/np.sum(props,(1,2), keepdims=True)
 #idx = 1000*np.ones(10, dtype=np.int64)
 # print("here2:",props)
 for user in trange(NUM_USERS):
     for j in range(NUM_LABELS):  # 4 labels for each users
         # l = (2*user+j)%10
-        l = (user + j) % 10
-        num_samples = int(props[l, user//int(NUM_USERS/10), j])
-        numran1 = random.randint(10, 200)
-        numran2 = random.randint(1, 10)
-        num_samples = (num_samples) * numran2 + numran1
-        if(NUM_USERS <= 20):
-            num_samples = num_samples * 2
+        l = ( user + j) % 10
+        #num_samples = int(props[l,user//int(NUM_USERS/10),j]) *10
+        num_samples = int(props[l, user, j]) * NUM_USERS
+        #num_samples = min(num_samples,200)
+        # print(num_samples)
         if idx[l] + num_samples < len(mnist_data[l]):
             X[user] += mnist_data[l][idx[l]:idx[l]+num_samples].tolist()
             y[user] += (l*np.ones(num_samples)).tolist()
             idx[l] += num_samples
-            print("check len os user:", user, j,
-                  "len data", len(X[user]), num_samples)
 
 print("IDX2:", idx) # counting samples for each labels
 

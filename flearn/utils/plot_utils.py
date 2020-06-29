fa886bc09929ef5701d53495f404cb0073543141
CharlieDinh <CharlieSex@156>
2020-06-29 12:10:29 +1000

    update average data

--- flearn/utils/plot_utils.py
+++ flearn/utils/plot_utils.py
@@ -128,10 +128,12 @@ def get_max_value_index(num_users=100, loc_ep1=5, Numb_Glob_Iters=10, lamb=[], l
 def plot_summary_mnist(num_users=100, loc_ep1=[], Numb_Glob_Iters=10, lamb=[], learning_rate=[],hyper_learning_rate=[], algorithms_list=[], batch_size=0,rho = [], dataset=""):
     Numb_Algs = len(algorithms_list)
 
-    glob_acc_, train_acc_, train_loss_ = get_training_data_value(num_users, loc_ep1, Numb_Glob_Iters, lamb, learning_rate, hyper_learning_rate, algorithms_list, batch_size, rho, dataset)
-    glob_acc =  average_smooth(glob_acc_, window='flat')
-    train_loss = average_smooth(train_loss_, window='flat')
-    train_acc = average_smooth(train_acc_, window='flat')
+    glob_acc, train_acc, train_loss = get_training_data_value(num_users, loc_ep1, Numb_Glob_Iters, lamb, learning_rate, hyper_learning_rate, algorithms_list, batch_size, rho, dataset)
+
+    #glob_acc_, train_acc_, train_loss_ = get_training_data_value(num_users, loc_ep1, Numb_Glob_Iters, lamb, learning_rate, hyper_learning_rate, algorithms_list, batch_size, rho, dataset)
+    #glob_acc =  average_smooth(glob_acc_, window='flat')
+    #train_loss = average_smooth(train_loss_, window='flat')
+    #train_acc = average_smooth(train_acc_, window='flat')
 
     for i in range(Numb_Algs):
         print(algorithms_list[i], "loss:", glob_acc[i].max())
@@ -153,7 +155,8 @@ def plot_summary_mnist(num_users=100, loc_ep1=[], Numb_Glob_Iters=10, lamb=[], l
     ax3.grid(True)
     #min = train_loss.min()
     min = train_loss.min() - 0.01
-    max = 0.5  # train_loss.max() + 0.01
+    #max = 0.5  
+    max = train_loss.max() + 0.01
     num_al = 2
 # Turn off axis lines and ticks of the big subplot
     ax.spines['top'].set_color('none')
@@ -457,6 +460,10 @@ def get_all_training_data_value(num_users=100, loc_ep1=5, Numb_Glob_Iters=10, la
 def average_data(num_users, loc_ep1, Numb_Glob_Iters, lamb,learning_rate, hyper_learning_rate, algorithms, batch_size, dataset, rho, times):
     glob_acc, train_acc, train_loss = get_all_training_data_value( num_users, loc_ep1, Numb_Glob_Iters, lamb, learning_rate, hyper_learning_rate, algorithms, batch_size, dataset, rho, times)
     # store average value to h5 file
+    glob_acc_data = np.average(glob_acc, axis=0)
+    train_acc_data = np.average(train_acc, axis=0)
+    train_loss_data = np.average(train_loss, axis=0)
+
     max_accurancy = []
     for i in range(times):
         max_accurancy.append(glob_acc[i].max())
@@ -480,8 +487,8 @@ def average_data(num_users, loc_ep1, Numb_Glob_Iters, lamb,learning_rate, hyper_
     alg = alg + "_" + "avg"
     if (len(glob_acc) != 0 &  len(train_acc) & len(train_loss)) :
         with h5py.File("./results/"+'{}.h5'.format(alg,loc_ep1), 'w') as hf:
-            hf.create_dataset('rs_glob_acc', data=glob_acc)
-            hf.create_dataset('rs_train_acc', data=train_acc)
-            hf.create_dataset('rs_train_loss', data=train_loss)
+            hf.create_dataset('rs_glob_acc', data=glob_acc_data)
+            hf.create_dataset('rs_train_acc', data=train_acc_data)
+            hf.create_dataset('rs_train_loss', data=train_loss_data)
             hf.close()
     return 0
